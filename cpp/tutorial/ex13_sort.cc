9596585ce94c5c9a78415131bb7033b623091416
abagogogo <rcnjko@yahoo.com.tw>
2023-06-20 09:45:52 +0800

    update sort

--- cpp/tutorial/ex13_sort.cc
+++ cpp/tutorial/ex13_sort.cc
@@ -1,24 +1,24 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
-    array<int, 3> a = {2, 1, 3};
-    vector<int> v(a.begin(), a.end());
+  array<int, 3> a = {2, 1, 3};
+  vector<int> v(a.begin(), a.end());
 
-    cout << "original: " << endl;
-    for (const auto& element : a) {
-        cout << element << ",";
-    }
-    cout << endl;
+  cout << "original: " << endl;
+  for (const auto& element : a) {
+    cout << element << ",";
+  }
+  cout << endl;
 
-    sort(v.begin(), v.end());
-    cout << "sorted: " << endl;
-    for (const auto& element : v) {
-        cout << element << ",";
-    }
-    cout << endl;
-    return 0;
+  sort(v.begin(), v.end());
+  cout << "sorted: " << endl;
+  for (const auto& element : v) {
+    cout << element << ",";
+  }
+  cout << endl;
+  return 0;
 }
