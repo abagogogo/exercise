c454a736ce453d26830ea4d06681069d22e3fd9a
abagogogo <rcnjko@yahoo.com.tw>
2023-06-20 09:45:09 +0800

    update sort

---
+++ cpp/tutorial/ex13_sort.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    array<int, 3> a = {2, 1, 3};
+    vector<int> v(a.begin(), a.end());
+
+    cout << "original: " << endl;
+    for (const auto& element : a) {
+        cout << element << ",";
+    }
+    cout << endl;
+
+    sort(v.begin(), v.end());
+    cout << "sorted: " << endl;
+    for (const auto& element : v) {
+        cout << element << ",";
+    }
+    cout << endl;
+    return 0;
+}
