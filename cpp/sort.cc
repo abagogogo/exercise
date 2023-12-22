95d949b3484632796ddbd27736ae9b660cf37168
abagogogo <rcnjko@yahoo.com.tw>
2023-12-22 10:22:39 +0800

    refacotr sort.cc with c++17

--- cpp/sort.cc
+++ cpp/sort.cc
@@ -1,77 +1,69 @@
-#include <algorithm>
+#include <array>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void insertion_sort(int *a, int n) {
-  // assume a[0 to i-1] are sorted, insert a[i].
-  for (int i = 1; i < n; ++i) {
-    int key = a[i];
-    int j;
-    for (j = i - 1; j >= 0; --j) {
-      if (a[j] <= key) break;
-      a[j + 1] = a[j];
+void insertion_sort(vector<int>& arr) {
+    // assume arr[0 to i-1] are sorted, insert arr[i].
+    for (size_t i = 1; i < arr.size(); ++i) {
+        int key = arr[i];
+        size_t j = i;
+        while (j > 0 && arr[j - 1] > key) {
+            arr[j] = arr[j - 1];
+            --j;
+        }
+        arr[j] = key;
     }
-    a[j + 1] = key;
-  }
 }
 
-void bubble_sort(int *a, int n) {
-  // bublle max of a[0 to i] to a[i]
-  for (int i = n - 1; i > 0; --i) {
-    for (int j = 0; j < i; ++j) {
-      if (a[j] > a[j + 1]) swap(a[j], a[j + 1]);
+void bubble_sort(vector<int>& arr) {
+    // bubble max of arr[0 to i] to arr[i].
+    for (size_t i = arr.size() -1; i > 0; --i) {
+        for (size_t j = 0; j < i; ++j) {
+            if (arr[j] > arr[j + 1]) swap(arr[j], arr[j+1]);
+        }
     }
-  }
 }
 
-void selection_sort(int *a, int n) {
-  // select min a[i to n-1] to a[i]
-  for (int i = 0; i < n - 1; ++i) {
-    int min_val = a[i];
-    int min_idx = i;
-    for (int j = i + 1; j < n; ++j) {
-      if (a[j] < min_val) {
-        min_val = a[j];
-        min_idx = j;
-      }
+void select_sort(vector<int>& arr) {
+    // select min of arr[i to n-1] to arr[i].
+    for (size_t i = 0; i < arr.size() -1; ++i) {
+        int min_val = arr[i];
+        size_t min_idx = i;
+        for (size_t j = i + 1; j < arr.size(); ++j) {
+            if (arr[j] < min_val) {
+                min_val = arr[j];
+                min_idx = j;
+            }
+        }
+        swap(arr[i], arr[min_idx]);
     }
-    swap(a[i], a[min_idx]);
-  }
 }
 
-int main(void) {
-  int input[] = {4, 3, 1, 5, 2};
-  int count = sizeof(input) / sizeof(input[0]);
-  int *sort1 = new int[count]();
-  int *sort2 = new int[count]();
-  int *sort3 = new int[count]();
+void dump(const vector<int>& arr) {
+    for (size_t i = 0; i < arr.size(); ++i) {
+        cout << "  arr[" << i << "] = " << arr[i] << endl;
+    }
+}
 
-  cout << "insertion sort" << endl;
-  for (int i = 0; i < count; ++i) {
-    sort1[i] = input[i];
-  }
-  insertion_sort(sort1, count);
-  for (int i = 0; i < count; ++i) {
-    cout << "sort1[" << i << "] = " << sort1[i] << endl;
-  }
+int main() {
+    array<int, 5> input{4, 3, 1, 5, 2};
 
-  cout << "bubble sort" << endl;
-  for (int i = 0; i < count; ++i) {
-    sort2[i] = input[i];
-  }
-  bubble_sort(sort2, count);
-  for (int i = 0; i < count; ++i) {
-    cout << "sort2[" << i << "] = " << sort2[i] << endl;
-  }
+    cout << "insertion sort" << endl;
+    vector<int> sort1(input.begin(), input.end());
+    insertion_sort(sort1);
+    dump(sort1);
 
-  cout << "selection sort" << endl;
-  for (int i = 0; i < count; ++i) {
-    sort3[i] = input[i];
-  }
-  selection_sort(sort3, count);
-  for (int i = 0; i < count; ++i) {
-    cout << "sort3[" << i << "] = " << sort3[i] << endl;
-  }
-  return 0;
+    cout << "bubble sort" << endl;
+    vector<int> sort2(input.begin(), input.end());
+    bubble_sort(sort2);
+    dump(sort2);
+
+    cout << "selection sort" << endl;
+    vector<int> sort3(input.begin(), input.end());
+    select_sort(sort3);
+    dump(sort3);
+
+    return 0;
 }
