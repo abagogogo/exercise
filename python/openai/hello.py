a265535df4ca68258dce5b9b0b5f7a3550d8b94c
abagogogo <rcnjko@yahoo.com.tw>
2023-12-25 11:11:27 +0800

    migrate hello.py to openai 1.0.0

--- python/openai/hello.py
+++ python/openai/hello.py
@@ -1,18 +1,26 @@
 import os
-import openai
+from openai import OpenAI
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 messages = [ {"role": "system", "content":
             "You are a intelligent assistant."} ]
+# gets API Key from environment variable OPENAI_API_KEY
+client = OpenAI()
+
 while True:
+    print("waiting user input ...")
     message = input("User : ")
+    print("user input \"%s\"" % message) 
     if message:
         messages.append(
             {"role": "user", "content": message},
         )
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
-    reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
-    messages.append({"role": "assistant", "content": reply})
+        print("before completions.create: %d" % len(messages))
+        for m in messages:
+            print(m)
+        completion = client.chat.completions.create(
+                        model="gpt-3.5-turbo",
+                        messages=messages)
+        print("after completions.create")
+        reply = completion.choices[0].message.content
+        print(f"ChatGPT: {reply}")
+        messages.append({"role": "assistant", "content": reply})
