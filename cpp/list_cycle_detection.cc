38dbdff4de1b49f7868d5c9dde34d30134f75d27
abagogogo <rcnjko@yahoo.com.tw>
2023-12-26 10:35:03 +0800

    refact list cycle detection with c++17

--- cpp/list_cycle_detection.cc
+++ cpp/list_cycle_detection.cc
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
+struct SinglyLinkedListNode {
+    SinglyLinkedListNode() {}
+    SinglyLinkedListNode* next;
+};
+
 bool has_cycle(SinglyLinkedListNode *head) {
   SinglyLinkedListNode *fast = head;
   SinglyLinkedListNode *slow = head;
@@ -11,4 +18,29 @@ bool has_cycle(SinglyLinkedListNode *head) {
     if (fast == slow) return true;
   }
   return false;
+}
+
+int main() {
+  auto head = std::make_unique<SinglyLinkedListNode>();
+  auto n1 =  std::make_unique<SinglyLinkedListNode>();
+  auto n2 =  std::make_unique<SinglyLinkedListNode>();
+  head.get()->next = n1.get();
+  n1.get()->next = n2.get();
+  n2.get()->next = head.get();
+
+  if (has_cycle(head.get())) {
+    cout << "Cycle found in the list" << endl;
+  } else {
+    cout << "No cycle found in the list" << endl;
+  }
+
+  cout << "Removing cycle..." << endl;
+  n2.get()->next = nullptr;
+  if (has_cycle(head.get())) {
+    cout << "Cycle found in the list" << endl;
+  } else {
+    cout << "No cycle found in the list" << endl;
+  }
+
+  return 0;
 }
