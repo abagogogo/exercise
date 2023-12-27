0ef0e9d591b12305e8ee2512d9a940026fea27a8
abagogogo <rcnjko@yahoo.com.tw>
2023-12-27 10:38:17 +0800

    check file existence in filesum.cc

--- cpp/filesum.cc
+++ cpp/filesum.cc
@@ -1,37 +1,30 @@
-#include <stdlib.h>
-
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <stdlib.h>
 
 using namespace std;
 
-int main(void) {
-#if 0
-    ifstream input("int_data.txt", ios::in | ios::binary);
-
-    int sum = 0;
-    char tmp;
-    while (input.read(&tmp, sizeof(tmp))) {
-        cout << tmp;
-        sum += tmp;
-    } 
-    cout << "----- file sum = " << sum << endl;
-    return 0;
-#endif
-
+int main() {
   string filename;
   int sum = 0;
   int num;
 
-  cout << "Please enter the interger file to parse: " << endl;
+  cout << "Please enter the file path containing lines of int numbers: " << endl;
   getline(cin, filename);
   if (filename.empty()) {
-    cout << "use defautl filename" << endl;
+    cout << "Using default filename" << endl;
     filename = "int_data.txt";
   }
 
   cout << "filename: " << filename << endl;
+  filesystem::path filepath(filename);
+  if (!filesystem::exists(filepath)) {
+    cout << "File does not exists!" << endl;
+    return 1;
+  }
+
   ifstream input(filename.c_str(), ios::in | ios::binary);
 
 #if 0  // ifstream getline()
@@ -44,6 +37,6 @@ int main(void) {
 #else
   while (input >> num) sum += num;
 #endif
-  cout << "sum = " << sum << endl;
+  cout << "sum = 0x" << hex << sum << endl;
   return 0;
 }
