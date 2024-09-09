bb7d577ceb920b0d86a9091aa7dc3568f00f9d3b
abagogogo <rcnjko@yahoo.com.tw>
2024-09-09 12:08:51 +0800

    refactor video_orb_desc_match.py

--- python/opencv/video_orb_desc_match.py
+++ python/opencv/video_orb_desc_match.py
@@ -1,68 +1,70 @@
 import cv2
 import sys
 
-
 def match_features(desc1, desc2):
   bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
   matches = bf.match(desc1, desc2)
-  matches = sorted(matches, key=lambda x:x.distance)
-  return len(matches)
+  return len(sorted(matches, key=lambda x:x.distance))
 
-def video_load(video):
-  cap = cv2.VideoCapture(video)
-  frame_end = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-  print("%s 1 has %d frames" % (video, frame_end))
-  return cap, frame_end
+def load_video(video_path):
+  cap = cv2.VideoCapture(video_path)
+  frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+  print(f"{video_path} has {frame_count} frames")
+  return cap, frame_count
 
-def frame_match_video(src_frame, dst_video):
-  dst_cap, dst_frame_end  = video_load(dst_video) 
-
+def extract_orb_features(frame):
   orb = cv2.ORB_create()
-  src_keypoints, src_descriptors = orb.detectAndCompute(src_frame, None)
+  return orb.detectAndCompute(frame, None)
+
+def process_video_frames(video_cap, frame_count):
+  while video_cap.isOpened():
+    ret, frame = video_cap.read()
+    if not ret:
+      print(f"Failed to read frame {frame_no}")
+      continue
+
+
+    frame_no = int(video_cap.get(cv2.CAP_PROP_POS_FRAMES))
+    if frame_no == frame_count:
+      break
+
+    yield frame, frame_no
+
+def match_frame_to_video(src_frame, dst_video):
+  dst_cap, dst_frame_count  = load_video(dst_video) 
+  src_keypoints, src_descriptors = extract_orb_features(src_frame)
 
   best_matches = 0
-  best_frame = src_frame
+  best_frame = None
   best_frame_no = 0
-  while dst_cap.isOpened():
-    ret, dst_frame = dst_cap.read()
 
-    frame_count = dst_cap.get(cv2.CAP_PROP_POS_FRAMES)
-    if frame_count == dst_frame_end:
-      break
-    if not ret:
-      print('Failed to read %s frame %d' % (dst_video, frame_count))
-      break
-
-    dst_keypoints, dst_descriptors = orb.detectAndCompute(dst_frame, None)
+  for dst_frame, dst_frame_no in process_video_frames(dst_cap, dst_frame_count):
+    dst_keypoints, dst_descriptors = extract_orb_features(dst_frame)
     matches = match_features(src_descriptors, dst_descriptors)
-    print(f'Matching to dst_video frame {frame_count}, matches={matches}')
+    print(f'Matching to dst_video frame {dst_frame_no}, matches={matches}')
+
     if matches > best_matches:
       best_matches = matches
       best_frame = dst_frame
-      best_frame_no = frame_count
-  print(f'Best matched frame {best_frame_no}, matches = {best_matches}')
+      best_frame_no = dst_frame_no
+
+  print(f"Best matched frame = {best_frame_no}, matches = {best_matches}")
+  dst_cap.release()
   return best_matches, best_frame
 
-def video_match(video1, video2):
-  cap, frame_end  = video_load(video1) 
+def match_videos(src_video, dst_video):
+  src_cap, src_frame_count  = load_video(src_video) 
 
-  while cap.isOpened():
-    ret, frame = cap.read()
+  for src_frame, src_frame_no in process_video_frames(src_cap, src_frame_count):
+    print(f'Matching to {src_video} frame {src_frame_no} to {dst_video}')
+    matches, matched_frame = match_frame_to_video(src_frame, dst_video)
+    cv2.imshow('Source Frame', src_frame)
+    cv2.imshow('Destination Frame', matched_frame)
 
-    frame_count = cap.get(cv2.CAP_PROP_POS_FRAMES)
-    if frame_count == frame_end:
-      break
-    if not ret:
-      print('Failed to read %s frame %d' % (video1, frame_count))
-      break
-
-    matches, matched_frame = frame_match_video(frame, video2)
-    cv2.imshow('Src Frame', frame)
-    cv2.imshow('Dst Frame', matched_frame)
     if cv2.waitKey(1000) == ord('q'):
       break
 
-  cap.release()
+  src_cap.release()
   cv2.destroyAllWindows()
 
 def main(argv=None):
@@ -70,9 +72,9 @@ def main(argv=None):
       argv=sys.argv
   print('OpenCV version:', cv2.__version__)
 
-  src = "IMG_0054.MOV"
-  dst = "IMG_0290.MOV"
-  video_match(src, dst)
+  src_video = "IMG_0054.MOV"
+  dst_video = "IMG_0290.MOV"
+  match_videos(src_video, dst_video)
 
 if __name__ == '__main__':
   sys.exit(main())
